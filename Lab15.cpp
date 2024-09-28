3889fa32d0e239414f38838c6a50cbfa0bdc31ba
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-09-27 20:01:03 -0700

    Timestamp

--- Lab15.cpp
+++ Lab15.cpp
@@ -35,20 +35,45 @@ int main()
 {
     // Initialize variables
     vector<Movie> movies = {};
+    // File to read from
+    const string INPUT_FILE_NAME = "input.txt";
+    ifstream inputFile;
+    string fileLine = "";
+    size_t lineCount = 0;
+
+    // Try opening file and output error to console if file is no good
+    inputFile.open(INPUT_FILE_NAME);
+    if (inputFile)
+    {
+        // Collect movie info from file
+        while (getline(inputFile, fileLine))
+        {
+            if (/* condition */)
+            {
+                /* code */
+            }
+            
+        }        
+
+        // CLOSE THE F-ING FILE
+        inputFile.close();
+    }
 
     // Output movies to console
     cout << "Movies:" << endl;
     for (size_t i = 0; i < movies.size(); i++)
     {
-        if (i % 4 == 0 && i != 0) cout << endl;
-        movies[i] = Movie().randomizeMovie(); // Return randomized color object
+        if (i % 4 == 0 && i != 0)
+            cout << endl;
+        movies[i] = Movie().randomizeMovie();                                     // Return randomized color object
         SetForegroundMovie(movies[i].getR(), movies[i].getG(), movies[i].getB()); // Change text color to RGB values
         cout << "Movie " << i << "::";
         ResetMovie();
-        cout << " " << movies[i].toString() << "\t";        
-    }      
+        cout << " " << movies[i].toString() << "\t";
+    }
 
-    cout << endl << endl; // Spacing
+    cout << endl
+         << endl; // Spacing
     return 0;
 }
 
@@ -70,17 +95,20 @@ Movie::Movie(string movieTitle, int year, string writer)
 void Movie::setTitle(string movieTitle)
 {
     // CONFIRM WRITING A GUARD STATEMENT LIKE THIS IS SAFE
-    if (movieTitle == "") throw invalid_argument("Movie must have a title.");
+    if (movieTitle == "")
+        throw invalid_argument("Movie must have a title.");
     title = movieTitle;
 }
 void Movie::setYearReleased(int year)
 {
-    if (year < 1800 || year > 2024) throw invalid_argument("Movie must have a valid year.");
+    if (year < 1800 || year > 2024)
+        throw invalid_argument("Movie must have a valid year.");
     yearReleased = year;
 }
 void Movie::setScreenWriter(string writer)
 {
-    if (writer == "") throw invalid_argument("Screen writer must be a valid name.");
+    if (writer == "")
+        throw invalid_argument("Screen writer must be a valid name.");
     screenWriter = writer;
 }
 string Movie::getTitle() const
